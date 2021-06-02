5a1939044a856dc17f7ac2c0ecc02d77f171613d
Vespan <57196133+Vespan@users.noreply.github.com>
2021-06-02 03:18:32 -0800

    Add files via upload

---
+++ Crash Tools [imgui].lua
@@ -0,0 +1,206 @@
+local imgui = require 'imgui'
+
+local encoding = require 'encoding'
+encoding.default = 'cp1251'
+u8 = encoding.UTF8
+
+local imgui_window = imgui.ImBool(false)
+
+d = [[
+{00AAFF}Gold Tools{FF0000} Вылетел!
+
+
+
+{a1932b}Что делать?
+{89ff2e}Попробуйте перезагрузить все lua скрипты.
+{89ff2e}Если у вас установлен reload_all.lua - то нажмите  {FF0000}лCONTROL + R
+{89ff2e}Попробуй удалить папку Gold Tools {6a7364}(в папке moonloader'e,но не забудьте выйти з игры!)
+
+{FF0000}Ничего не помогает?!
+{ff7d7d}Посмотри что за ошибка в moonloader.log {6a7364}(снизу){ff7d7d},сделай скриншот и скинь в тему со скриптом или разработчику в вк-дискорд
+{9100ff}vk.com/v3sp4n | v3sp4n#8544
+]]
+
+function main()
+
+	--sampRegisterChatCommand('/test', function() imgui_window.v = not imgui_window.v end)
+
+	while true do wait(0)
+		imgui.Process = imgui_window.v 
+	end
+end
+
+function EXPORTS.imgui(bool)
+	if bool == true then 
+		imgui_window.v = true 
+	else 
+		imgui_window.v = false 
+	end
+end
+
+function imgui.TextColor(text)
+    local width = imgui.GetWindowWidth()
+    local style = imgui.GetStyle()
+    local colors = style.Colors
+    local ImVec4 = imgui.ImVec4
+
+    local explode_argb = function(argb)
+        local a = bit.band(bit.rshift(argb, 24), 0xFF)
+        local r = bit.band(bit.rshift(argb, 16), 0xFF)
+        local g = bit.band(bit.rshift(argb, 8), 0xFF)
+        local b = bit.band(argb, 0xFF)
+        return a, r, g, b
+    end
+
+    local getcolor = function(color)
+        if color:sub(1, 6):upper() == 'SSSSSS' then
+            local r, g, b = colors[1].x, colors[1].y, colors[1].z
+            local a = tonumber(color:sub(7, 8), 16) or colors[1].w * 255
+            return ImVec4(r, g, b, a / 255)
+        end
+        local color = type(color) == 'string' and tonumber(color, 16) or color
+        if type(color) ~= 'number' then return end
+        local r, g, b, a = explode_argb(color)
+        return imgui.ImColor(r, g, b, a):GetVec4()
+    end
+
+    local render_text = function(text_)
+        for w in text_:gmatch('[^\r\n]+') do
+            local textsize = w:gsub('{.-}', '')
+            local text_width = imgui.CalcTextSize(u8(textsize))
+            imgui.SetCursorPosX( width / 2 - text_width .x / 2 )
+            local text, colors_, m = {}, {}, 1
+            w = w:gsub('{(......)}', '{%1FF}')
+            while w:find('{........}') do
+                local n, k = w:find('{........}')
+                local color = getcolor(w:sub(n + 1, k - 1))
+                if color then
+                    text[#text], text[#text + 1] = w:sub(m, n - 1), w:sub(k + 1, #w)
+                    colors_[#colors_ + 1] = color
+                    m = n
+                end
+                w = w:sub(1, n - 1) .. w:sub(k + 1, #w)
+            end
+            if text[0] then
+                for i = 0, #text do
+                    imgui.TextColored(colors_[i] or colors[1], u8(text[i]))
+                    imgui.SameLine(nil, 0)
+                end
+                imgui.NewLine()
+            else
+                imgui.Text(u8(w))
+            end
+        end
+    end
+    render_text(text)
+end
+
+function imgui.OnDrawFrame()
+	apply_custom_style()
+	sw, sh = getScreenResolution()
+	if imgui_window.v then
+	    imgui.SetNextWindowPos(imgui.ImVec2(sw / 2, sh / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))		
+		imgui.SetNextWindowSize(imgui.ImVec2(875, 550), imgui.Cond.FirstUseEver)
+		imgui.Begin(u8'Crash!', imgui_window, imgui.WindowFlags.NoCollapse + imgui.WindowFlags.NoBringToFrontOnFocus)
+        if imgui.Button(u8'Закрыть', imgui.ImVec2(-1, 20)) then imgui_window.v = false end
+		imgui.TextColor(d)
+        if imgui.CollapsingHeader(u8'У меня нету reload_all.lua') then 
+            if imgui.Button(u8'Ну,тогда нажми сюда шоб перезагрузить всё .lua', imgui.ImVec2(-1, 35)) then 
+                reloadScripts()
+            end
+            imgui.Separator()
+        end
+        if imgui.CollapsingHeader(u8'Moonloader Log') then
+
+            --imgui.SetWindowFontScale(1.2)
+            --imgui.TextColor('Ничего не помогает?!\nПосмотри что за ошибка в moonloader.log (снизу),сделай скриншот и скинь в тему со скриптом или разработчику в вк-дискорд')
+
+            log = io.open('moonloader/moonloader.log', 'r+')             
+            for lines in log:lines() do
+               imgui.Text((lines))
+            end            
+
+        end
+		imgui.End()
+	end
+end
+
+
+
+function apply_custom_style()
+    imgui.SwitchContext()
+    local style = imgui.GetStyle()
+    local colors = style.Colors
+    local clr = imgui.Col
+    local ImVec4 = imgui.ImVec4
+
+    style.WindowRounding = 2.0
+    style.WindowTitleAlign = imgui.ImVec2(0.5, 0.84)
+    style.ChildWindowRounding = 2.0
+    style.FrameRounding = 2.0
+    style.ItemSpacing = imgui.ImVec2(5.0, 4.0)
+    style.ScrollbarSize = 13.0
+    style.ScrollbarRounding = 0
+    style.GrabMinSize = 8.0
+    style.GrabRounding = 1.0
+
+    colors[clr.FrameBg]                = ImVec4(0.48, 0.16, 0.16, 0.54)
+    colors[clr.FrameBgHovered]         = ImVec4(0.98, 0.26, 0.26, 0.40)
+    colors[clr.FrameBgActive]          = ImVec4(0.98, 0.26, 0.26, 0.67)
+    colors[clr.TitleBg]                = ImVec4(0.04, 0.04, 0.04, 1.00)
+    colors[clr.TitleBgActive]          = ImVec4(0.48, 0.16, 0.16, 1.00)
+    colors[clr.TitleBgCollapsed]       = ImVec4(0.00, 0.00, 0.00, 0.51)
+    colors[clr.CheckMark]              = ImVec4(0.98, 0.26, 0.26, 1.00)
+    colors[clr.SliderGrab]             = ImVec4(0.88, 0.26, 0.24, 1.00)
+    colors[clr.SliderGrabActive]       = ImVec4(0.98, 0.26, 0.26, 1.00)
+    colors[clr.Button]                 = ImVec4(0.98, 0.26, 0.26, 0.40)
+    colors[clr.ButtonHovered]          = ImVec4(0.98, 0.26, 0.26, 1.00)
+    colors[clr.ButtonActive]           = ImVec4(0.98, 0.06, 0.06, 1.00)
+    colors[clr.Header]                 = ImVec4(0.98, 0.26, 0.26, 0.31)
+    colors[clr.HeaderHovered]          = ImVec4(0.98, 0.26, 0.26, 0.80)
+    colors[clr.HeaderActive]           = ImVec4(0.98, 0.26, 0.26, 1.00)
+    colors[clr.Separator]              = colors[clr.Border]
+    colors[clr.SeparatorHovered]       = ImVec4(0.75, 0.10, 0.10, 0.78)
+    colors[clr.SeparatorActive]        = ImVec4(0.75, 0.10, 0.10, 1.00)
+    colors[clr.ResizeGrip]             = ImVec4(0.98, 0.26, 0.26, 0.25)
+    colors[clr.ResizeGripHovered]      = ImVec4(0.98, 0.26, 0.26, 0.67)
+    colors[clr.ResizeGripActive]       = ImVec4(0.98, 0.26, 0.26, 0.95)
+    colors[clr.TextSelectedBg]         = ImVec4(0.98, 0.26, 0.26, 0.35)
+    colors[clr.Text]                   = ImVec4(1.00, 1.00, 1.00, 1.00)
+    colors[clr.TextDisabled]           = ImVec4(0.50, 0.50, 0.50, 1.00)
+    colors[clr.WindowBg]               = ImVec4(0.06, 0.06, 0.06, 0.94)
+    colors[clr.ChildWindowBg]          = ImVec4(1.00, 1.00, 1.00, 0.00)
+    colors[clr.PopupBg]                = ImVec4(0.08, 0.08, 0.08, 0.94)
+    colors[clr.ComboBg]                = colors[clr.PopupBg]
+    colors[clr.Border]                 = ImVec4(0.43, 0.43, 0.50, 0.50)
+    colors[clr.BorderShadow]           = ImVec4(0.00, 0.00, 0.00, 0.00)
+    colors[clr.MenuBarBg]              = ImVec4(0.14, 0.14, 0.14, 1.00)
+    colors[clr.ScrollbarBg]            = ImVec4(0.02, 0.02, 0.02, 0.53)
+    colors[clr.ScrollbarGrab]          = ImVec4(0.31, 0.31, 0.31, 1.00)
+    colors[clr.ScrollbarGrabHovered]   = ImVec4(0.41, 0.41, 0.41, 1.00)
+    colors[clr.ScrollbarGrabActive]    = ImVec4(0.51, 0.51, 0.51, 1.00)
+    colors[clr.CloseButton]            = ImVec4(0.41, 0.41, 0.41, 0.50)
+    colors[clr.CloseButtonHovered]     = ImVec4(0.98, 0.39, 0.36, 1.00)
+    colors[clr.CloseButtonActive]      = ImVec4(0.98, 0.39, 0.36, 1.00)
+    colors[clr.PlotLines]              = ImVec4(0.61, 0.61, 0.61, 1.00)
+    colors[clr.PlotLinesHovered]       = ImVec4(1.00, 0.43, 0.35, 1.00)
+    colors[clr.PlotHistogram]          = ImVec4(0.90, 0.70, 0.00, 1.00)
+    colors[clr.PlotHistogramHovered]   = ImVec4(1.00, 0.60, 0.00, 1.00)
+    colors[clr.ModalWindowDarkening]   = ImVec4(0.80, 0.80, 0.80, 0.35)
+
+    style.WindowPadding = imgui.ImVec2(4, 4)
+    style.WindowRounding = 7
+    style.ChildWindowRounding = 7
+    style.FramePadding = imgui.ImVec2(5, 3)
+    style.FrameRounding = 10
+    style.ItemSpacing = imgui.ImVec2(5, 4)
+    style.ItemInnerSpacing = imgui.ImVec2(4, 4)
+    style.IndentSpacing = 21
+    style.ScrollbarSize = 13
+    style.ScrollbarRounding = 10
+    style.GrabMinSize = 8
+    style.GrabRounding = 10
+    style.WindowTitleAlign = imgui.ImVec2(0.5, 0.5)
+    style.ButtonTextAlign = imgui.ImVec2(0.5, 0.5)  
+
+end
